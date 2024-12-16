a62f60c7e8ca197fe5745dbc010ee34829b38ccd
FidanAsadova55555 <fidanra@code.edu.az>
2024-12-17 03:40:38 +0400

    half of home is ready

--- src/js/app.js
+++ src/js/app.js
@@ -30,30 +30,40 @@ window.addEventListener("resize", () => {
 
 
 ///////////////////////////////////////////////
-let lastScrollPosition = 0;
-let ticking = false;
-const introImage = document.querySelector('.introimage');
 
+window.addEventListener('scroll', () => {
+  const introImage = document.querySelector('.introimage');
+  const scrollY = window.scrollY; 
+  introImage.style.transform =`translateY(${scrollY * -0.3}px)`;});
 
-window.addEventListener("scroll", () => {
-    const currentScrollPosition = window.scrollY;
-
-    if (!ticking) {
-        window.requestAnimationFrame(() => {
-            if (currentScrollPosition < lastScrollPosition) {
-                console.log("Scrolling up");
-                introImage.classList.remove('animate');
-            }
-            else{
-                introImage.classList.add('animate');
-
-            }
-            lastScrollPosition = currentScrollPosition;
-            ticking = false;
-        });
-        ticking = true;
-    }
+//////////////////////////////////////////////////////////////
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    900: {
+      slidesPerView: "auto",
+      centeredSlides: true, 
+    },
+    0: {
+      slidesPerView: 1,
+      centeredSlides: false,
+      spaceBetween: 10,
+    },
+  },
+  rewind: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
+console.log(swiper);
 //////////////////////////////////////////////////////////////
 const instance = axios.create({
     baseURL: "http://localhost:3001",
@@ -131,4 +141,151 @@ const instance = axios.create({
   fetchData("/accordionData", (data) => {
     renderAccordionData(data);
   });
+  /////////////////////////////////////////////////
+ 
+  const swiperD = document.querySelector(".swiper-wrapper");
+  if (!swiperD) {
+    console.error("Element with classname 'swiper-wrapper' not found");
+  }
+  const renderSwiperData = async (data) => {
+    if (data && swiperD) {
+      data.forEach((swiperData, index) => {
+        const swiperHtml = `
+          <div key="${index}" class="swiper-slide">
+<div class="slidecontainer">
+    <div class="row justify-content-between">
+        <div class="col-lg-7 col-md-12">
+           
+                        <div class="values-box-title-wrap"><h2 class="values-box-title">"${swiperData?.header}"
+                        </h2></div> 
+                        <div class="values-box-text-wrap"><p class="values-box-text">${swiperData?.text}</p></div>
+                        <div class="values-box-title-wrap"><h2 class="values-box-title">${swiperData?.name}
+                        </h2></div> 
+                        <div class="values-box-text-wrap"><p class="values-box-text">${swiperData?.location}
+
+                        </p></div>
+                    </div>
+        <div class="col-lg-4 col-md-12">
+            <div class="testimonials-thumb-wrap"><img src="${swiperData?.img}" alt="A man in a suit and tie smiling." class="testimonials-thumb"></div>
+        </div>
+    </div>
+</div>
+
+
+
+                          </div>
+                          
+        `;
+        swiperD.innerHTML += swiperHtml;
+      });
   
+     
+    } else {
+      console.error("No data or target element to render");
+    }
+  };
+  
+  fetchData("/swiperData", (data) => {
+    renderSwiperData(data);
+  });
+  ///////////////////////////////////////////////////////////////////
+
+const today = dayjs().format('MMMM D, YYYY');
+
+
+
+  
+    
+    
+    
+    ////////////////////////////////////////////////////////////////////////
+    let minutesAgo;
+    const blogsui = document.getElementById("blogsui");
+    if (!blogsui) {
+      console.error("Element with classname 'swiper-wrapper' not found");
+    }
+    const renderbloqsuiData = async (data) => {
+      if (data && blogsui) {
+        data.forEach((blog) => {
+            
+        let exitTime = localStorage.getItem('exitTime'); 
+        const currentTime = new Date();
+        
+        if (!exitTime) {
+            exitTime = currentTime.toISOString(); 
+            localStorage.setItem('exitTime', exitTime);
+        } else {
+            localStorage.setItem('exitTime', currentTime.toISOString());
+        }
+        const exitTimeParsed = new Date(exitTime);
+const differenceInMinutes = Math.round((currentTime - exitTimeParsed) / 1000 / 60); 
+
+ minutesAgo = differenceInMinutes > 0 ? differenceInMinutes : 0;
+          const blogsuiHtml = `
+            <div id="responsiveblog"  class="col-lg-4 col-md-6 col-sm-12">
+                            <div class="blogcontainer">
+                                <div class="blogimg">
+                                    <a href="${blog?.href}">
+                                        <img src="${blog?.blogimg}" alt="">  
+
+                                    </a>
+                                </div>
+                               <div class="values-box-title-wrap">
+                                <a  href="${blog?.href}">
+                                    <h2 class="values-box-title">${blog?.txt}</h2>
+
+                                </a>
+                             </div> 
+                             <div class="blogdata">
+                                <p class="date"><i class="ri-calendar-2-line"></i>${today}                               </p>
+                                <p class="date"><i class="ri-time-line"></i>${minutesAgo} min read</p>
+                             </div>
+                             <div class="btn">
+                                <a href="${blog?.href}">Read more</a>
+                             </div>
+                            </div>
+                        </div>`;
+          blogsui.innerHTML += blogsuiHtml;
+        });
+    
+       
+      } else {
+        console.error("No data or target element to render");
+      }
+    };
+    document.addEventListener('DOMContentLoaded', () => {
+        const minutesAgoElement = document.querySelector('.minutes-ago');
+        if (minutesAgoElement) {
+            minutesAgoElement.innerHTML = `<i class="ri-time-line"></i>${minutesAgo} min read`;
+        }
+    });
+    fetchData("/blog", (data) => {
+      renderbloqsuiData(data);
+    });
+    ////////////////////////////////////////////////////////////////////////////////
+
+const dynamicimg = document.getElementById("dynamic");
+const procesLeft = document.querySelector(".procesleft");
+let images = [];
+
+fetchData("/scrollimages", (data) => {
+  images = data.map(item => item.img); });
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    const rect = procesLeft.getBoundingClientRect();
+    const top = rect.top + window.scrollY;
+    const bottom = rect.bottom + window.scrollY;
+
+    if (images.length > 0 && scrollY >= top && scrollY <= bottom) {
+        const normalizedScroll = scrollY - top;
+
+        const totalImages = images.length;
+        const index = Math.min(Math.floor(normalizedScroll / 400), totalImages - 1);
+
+        dynamicimg.src = images[index];
+    } else if (scrollY < top) {
+        dynamicimg.src = images[0];
+    }
+});
---
+++ src/js/blogdetail.js
@@ -0,0 +1,171 @@
+const today = dayjs().format('MMMM D, YYYY');
+
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+        const blogId = urlParams.get('id');
+
+        fetch(`http://localhost:3001/blogdetail/${blogId}`)
+    .then(response => response.json())
+    .then(data => {
+        const blogdetail = document.getElementById('blogdetail');
+
+        if (blogdetail) {
+          blogdetail.innerHTML += `
+                <div class="container-fluid p-0">
+                <div id="Blog-Info">
+<h1>${data?.title}
+</h1>
+<p>${data?.description}
+</p>
+<div class="blogdata">
+    <p class="date"><i class="ri-calendar-2-line"></i>${today}</p>
+    <p class="date minutes-ago"><i class="ri-time-line"></i>${minutesAgo} min read</p>
+ </div>
+ <div class="writer">
+    <div class="hisphoto">
+        <img src="${data?.writerimg}" alt="">
+    </div>
+    <div class="hisdata">
+        <p class="authorname" style="margin: 0;">Albert Flores</p>
+        <p style="margin: 0;font-weight: 500">Blog Writer</p>
+    </div>
+ </div>
+ <div class="blogphoto">
+    <img src="${data?.img}" alt="">
+ </div>
+ <div class="row">
+    <div class="col-lg-9 col-md-12 col-sm-12">
+        <h1 class="blogdetailheader">
+            Understanding housing stocks
+
+        </h1>
+        <p>Stay updated with the latest industry trends, learn from real-world case studies, and discover innovative approaches to grow your brand and reach your business goals. Our team of experts is committed to empowering you with the tools and resources you need to thrive in today's competitive landscape.
+
+        </p>
+        <p>Follow the to may with deceleration has if of make called did managers rewritten him, they the made story immune theory is reported and beginning for build been did the I but the all in head we harmonics. Between municipal morals, had her fresh with parts the better shortcuts. Stick sleeping have of h    uman of as how let's snow out but on use what's country. Go point with was that success and there first software their specially attribute I cheerful, events, when and especially cheek, we've did and about depend as people office. Went as carefully texts feedback the equation.
+
+        </p>
+        <div class="blogtextphoto">
+<img src="https://cdn.prod.website-files.com/6704d32b156a64ece7cc9779/67091d608db09d20d054551b_67091cdcc63ef691b7483c88_d-blog-1.jpeg" alt="">
+        </div>
+   <h1 class="blogdetailheader">Key principles of green building
+</h1>
+<p>Is easy movement them, for harder your a in duty the except when of interfaces had is screen state be accuse own set the creating how have him, on the into authentic box funds time a would fellow go sort to seemed uninitiated morning the distressed a encouraged because affected.
+
+</p>
+<blockquote>“Options it deeply, the is for and management-science days, such to a expecting was hungrier much one may for titles in original soon the as to relays in not the would the of troubled economics, of how it o'clock evaluation due to us, remote either him furnished at the be choose his our to not the own.”</blockquote>
+<h1 class="blogdetailheader">Embracing a sustainable future
+</h1>
+<p>The classes and uneasiness, his ticking service, what something it bear extended had sooner sort we're of one possible to found switching the him, over coming fully hard such to least, would with poverty if time reached decorated boss in and, early rest was a the wrong and of to life still some copy times of leave certainly salesman quarter done came but very english length observed, could told it's in myself picture some two like him in blind and from through cache legs.
+
+</p>
+<p>Lack value from never started either the little it made the of and several your said legs, in generally after does to then, attempt, that importance, suppose joke. Location back been he of their would or of its have met her though not with project will two the in two and family time.
+
+</p>
+<div class="blogtextphoto">
+    <img src="https://cdn.prod.website-files.com/6704d32b156a64ece7cc9779/67091d608db09d20d0545518_67091d21e3b5f95fb3f72f04_d-blog-2.jpeg" alt="">
+</div>
+<h1 class="blogdetailheader">Identify emerging trends
+</h1>
+<p>Fresh proper didn't how the of such I that a ill for to yet between them. Last his process you so dissolute in was advised is far all line much other it the being and out multi doesn't attentive never but and the and to completely.
+
+</p>
+
+   
+   
+   
+   
+   
+    </div>
+ </div>
+                </div>
+            </div>
+            `;
+        } else {
+            console.error('Element with id "blogInfo" not found');
+        }
+    })
+    .catch(error => console.error('Error fetching blog data:', error));
+   
+    
+    
+    
+    ////////////////////////////////////////////////////////////////////////
+    const instance = axios.create({
+        baseURL: "http://localhost:3001",
+        timeout: 3000,
+      });
+      
+      const fetchData = async (url, cb) => {
+        try {
+          const res = await instance.get(url);
+          cb(res.data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      let minutesAgo;
+    const blogsui = document.getElementById("blogsui");
+    if (!blogsui) {
+      console.error("Element with classname 'swiper-wrapper' not found");
+    }
+    const renderbloqsuiData = async (data) => {
+      if (data && blogsui) {
+        data.forEach((blog) => {
+            
+        let exitTime = localStorage.getItem('exitTime'); 
+        const currentTime = new Date();
+        
+        if (!exitTime) {
+            exitTime = currentTime.toISOString(); 
+            localStorage.setItem('exitTime', exitTime);
+        } else {
+            localStorage.setItem('exitTime', currentTime.toISOString());
+        }
+        const exitTimeParsed = new Date(exitTime);
+const differenceInMinutes = Math.round((currentTime - exitTimeParsed) / 1000 / 60); 
+
+ minutesAgo = differenceInMinutes > 0 ? differenceInMinutes : 0;
+          const blogsuiHtml = `
+            <div id="responsiveblog"  class="col-lg-4 col-md-6 col-sm-12">
+                            <div class="blogcontainer">
+                                <div class="blogimg">
+                                    <a href="${blog?.href}">
+                                        <img src="${blog?.blogimg}" alt="">  
+
+                                    </a>
+                                </div>
+                               <div class="values-box-title-wrap">
+                                <a  href="${blog?.href}">
+                                    <h2 class="values-box-title">${blog?.txt}</h2>
+
+                                </a>
+                             </div> 
+                             <div class="blogdata">
+                                <p class="date"><i class="ri-calendar-2-line"></i>${today}                               </p>
+                                <p class="date"><i class="ri-time-line"></i>${minutesAgo} min read</p>
+                             </div>
+                             <div class="btn">
+                                <a href="${blog?.href}">Read more</a>
+                             </div>
+                            </div>
+                        </div>`;
+          blogsui.innerHTML += blogsuiHtml;
+        });
+    
+       
+      } else {
+        console.error("No data or target element to render");
+      }
+    };
+    document.addEventListener('DOMContentLoaded', () => {
+        const minutesAgoElement = document.querySelector('.minutes-ago');
+        if (minutesAgoElement) {
+            minutesAgoElement.innerHTML = `<i class="ri-time-line"></i>${minutesAgo} min read`;
+        }
+    });
+    fetchData("/blog", (data) => {
+      renderbloqsuiData(data);
+    });
